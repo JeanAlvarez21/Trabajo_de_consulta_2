3473febce7e5454df2ad5cbd8f9f818bde1c245a
Jean Alvarez <116139695+JeanAlvarez21@users.noreply.github.com>
2024-01-17 22:25:03 -0500

    codigo 1.0

---
+++ TallerInvestigacion2.scala
@@ -0,0 +1,28 @@
+import akka.actor.{Actor, ActorSystem, Props}
+
+// Definición de mensajes
+case class Saludo(nombre: String)
+
+// Definición de un actor
+class SaludoActor extends Actor {
+  def receive: Receive = {
+    case Saludo(nombre) =>
+      println(s"Hola, $nombre!")
+  }
+}
+
+object ProgramaReactivo {
+  def main(args: Array[String]): Unit = {
+    // Creación del sistema de actores
+    val system = ActorSystem("SistemaSaludo")
+
+    // Creación de un actor
+    val saludoActor = system.actorOf(Props(classOf[SaludoActor]), "SaludoActor")
+
+    // Envío de un mensaje al actor
+    saludoActor ! Saludo("Juan")
+
+    // Cierre del sistema de actores
+    system.terminate()
+  }
+}
